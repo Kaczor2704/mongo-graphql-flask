b0ce1e3c00be680e68b886da555826674e8c372e
Marcin Baryłka <marcin.barylka@gmail.com>
2018-09-23 16:38:45 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python, GraphQL, MongoDB
+
+Prototyp, część Pythonową należy przepisać na PHP albo node-js, jeśli uznamy to za stosowne.
---
+++ schema.py
@@ -0,0 +1,38 @@
+import graphene
+from graphene.relay import Node
+
+from graphene_mongo import MongoengineObjectType, MongoengineConnectionField
+
+from models import (Department as DepartmentModel, Role as RoleModel,
+                    Employee as EmployeeModel)
+
+
+class Department(MongoengineObjectType):
+
+    class Meta:
+        model = DepartmentModel
+        interfaces = (Node, )
+
+
+class Role(MongoengineObjectType):
+
+    class Meta:
+        model = RoleModel
+        interfaces = (Node, )
+
+
+class Employee(MongoengineObjectType):
+
+    class Meta:
+        model = EmployeeModel
+        interfaces = (Node, )
+
+
+class Query(graphene.ObjectType):
+    node = Node.Field()
+    all_employees = MongoengineConnectionField(Employee)
+    all_role = MongoengineConnectionField(Role)
+    role = graphene.Field(Role)
+
+
+schema = graphene.Schema(query=Query, types=[Department, Employee, Role])
